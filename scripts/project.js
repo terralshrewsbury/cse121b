35311d022a556ad7829e07f81ca1cb8bad0d9b13
Turrtle0612 <123220471+Turrtle0612@users.noreply.github.com>
2023-10-21 22:03:04 -0600

    working on my final project for javascript

---
+++ scripts/project.js
@@ -0,0 +1,31 @@
+const promptTextBox = document.querySelector("#prompt-text-box");
+let promptList = [];
+
+const getPrompts = async() => {
+    try {
+        const response = await fetch("prompts.json");
+        const data = await response.json();
+        promptList = Array.isArray(data.prompts) ? data.prompts : [];
+        console.log(promptList);
+    } catch (error) {
+        console.error("Error fetching prompts:", error);
+    }
+};
+
+const randomPrompt = (prompts) => {
+    const randomIndex = Math.floor(Math.random()* prompts.length);
+    return prompts[randomIndex];
+};
+
+const displayPrompt = async () => {
+    await getPrompts();
+    console.log(promptList);
+    let prompt = randomPrompt(promptList);
+    promptTextBox.textContent = prompt;  
+};
+
+document.getElementById('prompt-button-box').addEventListener('click', () =>{ 
+    displayPrompt();
+});
+
+displayPrompt();
