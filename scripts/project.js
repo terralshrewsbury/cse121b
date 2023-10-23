cac43dd4e87eac081aae31189ab486c3a2d03e45
Turrtle0612 <123220471+Turrtle0612@users.noreply.github.com>
2023-10-23 11:40:58 -0600

    I finished my Javascript project.

--- scripts/project.js
+++ scripts/project.js
@@ -1,4 +1,9 @@
 const promptTextBox = document.querySelector("#prompt-text-box");
+const promptresponse = document.querySelector("#text-box");
+const data = promptresponse.value;
+const entries = []
+const textBox = document.querySelector 
+
 let promptList = [];
 
 const getPrompts = async() => {
@@ -6,12 +11,11 @@ const getPrompts = async() => {
         const response = await fetch("https://raw.githubusercontent.com/terralshrewsbury/cse121b/main/prompts.json");
         if (!response.ok){
             throw new Error('Not fetching well.');
-            console.log('error');
         }
         const data = await response.json();
         promptList = Array.isArray(data.prompts) ? data.prompts : [];
         console.log(promptList);
-        displayPrompt
+        displayPrompt;
     } catch (error) {
         console.error("Error fetching prompts:", error);
     }
@@ -25,11 +29,38 @@ const randomPrompt = (prompts) => {
 const displayPrompt = () => {
     console.log(promptList);
     let prompt = randomPrompt(promptList);
-    promptTextBox.textContent = prompt;  
+    promptTextBox.textContent = prompt;
+    promptresponse.value = "";
 };
 
-document.getElementById('prompt-button-box').addEventListener('click', () =>{ 
+document.getElementById("prompt-button-box").addEventListener('click', (event) =>{ 
     displayPrompt();
+    event.preventDefault();
 });
+
+document.getElementById("delete-button").addEventListener("click", (event) => {
+    promptresponse.value="";
+    event.preventDefault();
+})
+
+const save = () =>{
+    const currentDate = new Date();
+    const entrie = `${currentDate}: ${promptTextBox.textContent}- ${promptresponse.value}`;
+    promptresponse.value = "";
+    return entrie;
+};
+
+document.getElementById("save-button").addEventListener("click",(event) =>{
+addtohtml();
+event.preventDefault();
+})
+
+const addtohtml = () =>{
+    const newEntry = document.createElement("li");
+    newEntry.textContent = save();
+
+    const list = document.getElementById("entrylog");
+    list.appendChild(newEntry);
+};
 
 getPrompts();
